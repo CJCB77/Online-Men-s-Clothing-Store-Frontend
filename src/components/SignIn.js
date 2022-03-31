d720e2036709272acd0e4f4960ff1013df0c85da
Carlos Coral <coral.johnny.2@gmail.com>
2022-03-31 01:26:12 -0500

    Added sign in modal

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import signInStyles from "../styles/signin.module.css"
+import cross from "../imgs/crossBlue.svg"
 
 export default function SignIn() {
+
+  
+
   return (
     <section className={signInStyles.signIn}>
       <article className={signInStyles.container}>
+        <img src={cross} alt="" className={signInStyles.close}/>
         <h1>Sign In</h1>
         <h3>Sign into your account for discounts, rewards and orders.</h3>
         <div className={signInStyles.form__control}>
@@ -16,8 +21,8 @@ export default function SignIn() {
           <input type="password" name="password" id="password" />
         </div>
         <a href="#" className={signInStyles.recover}>Forgot password?</a>
-        <a href="#" className={signInStyles.signIn_btn}>Sign In</a>
-        <a href="#" className={signInStyles.regiter_btn}>Become a member</a>
+        <a href="#" className={signInStyles.signIn__btn}>Sign in</a>
+        <a href="#" className={signInStyles.register__btn}>Become a member</a>
       </article>
     </section>
   )
