bba96999cffa8e488172a12e9e4d1406a5ae13bf
Carlos Coral <coral.johnny.2@gmail.com>
2022-03-31 00:14:09 -0500

    Added newsletter suscription section

---
+++ src/components/Newsletter.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import newsletterStyles from "../styles/newsletter.module.css"
+import send from "../imgs/send.svg"
+
+export default function Newsletter() {
+
+  const [email, setEmail] = useState("")
+  
+
+  function handleChange(e) {
+    const {value} = e.target
+    setEmail(value)
+
+
+  }
+
+  function handleClick(e) {
+      console.log(email)
+  }  
+
+  return (
+    <section className={newsletterStyles.newsletter}>
+        <h1>Join Our Newsletter!</h1>
+        <h2>Get timely updates for discounts or new products.</h2>
+        
+        <div className={newsletterStyles.suscriptionBox}>
+            <input type="text" placeholder='youremail@email.com' 
+              name="email" value={email} onChange={handleChange}/>
+            <button onClick={handleClick}> <img src={send} alt="" /> </button>
+        </div>
+
+    </section>  
+  )
+}
